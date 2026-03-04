1f69ebdc5a01b28f7ee7ab7002ff5a1bfc33c61c
James Rittgers <JamesR2010@protonmail.com>
2026-03-03 19:51:32 -0500

    Go back to simpler times...

--- james-vision/src/main/java/frc/robot/subsystems/Vision/VisionSim.java
+++ james-vision/src/main/java/frc/robot/subsystems/Vision/VisionSim.java
@@ -7,24 +7,20 @@ import org.photonvision.simulation.VisionSystemSim;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 
 public class VisionSim {
 
-    // The PhotonCamera used in the real robot code.
-    PhotonCamera camera = new PhotonCamera("cameraName");
-
     // The simulated camera properties
     SimCameraProperties cameraProp = new SimCameraProperties();
 
+    // The PhotonCamera used in the real robot code.
+    PhotonCamera camera = new PhotonCamera("cameraName");
+
     // The simulation of this camera. Its values used in real robot code will be updated.
     PhotonCameraSim cameraSim = new PhotonCameraSim(camera, cameraProp);
-
-
-    // Translation Stuff
 
     // Our camera is mounted 0.1 meters forward and 0.5 meters up from the robot pose,
     // (Robot pose is considered the center of rotation at the floor level, or Z = 0)
@@ -35,10 +31,11 @@ public class VisionSim {
 
     // Here's some code copied from the PhotonLib reference
     // A vision system sim labelled as "main" in NetworkTables
-    public VisionSystemSim visionSim = new VisionSystemSim("main");
+    VisionSystemSim visionSim = new VisionSystemSim("main");
 
     // The layout of AprilTags which we want to add to the vision system
     AprilTagFieldLayout tagLayout =  AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField); 
+
 
     public void visionSimulate(){
 
@@ -54,10 +51,6 @@ public class VisionSim {
 
         // Add this camera to the vision system simulation with the given robot-to-camera transform.
         visionSim.addCamera(cameraSim, robotToCamera);
-    }
-
-    public Pose2d update(){
-        return null;
     }
 
 
