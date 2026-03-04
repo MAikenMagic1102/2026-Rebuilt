87d89473305641222c59c92adcf758a2835a067c
James Rittgers <JamesR2010@protonmail.com>
2026-03-03 19:55:23 -0500

    Reapply "Refactoring"
    This reverts commit 2f11677f7303fbc7b9268a8fb06bcfa0001890bf.

--- james-vision/src/main/java/frc/robot/Robot.java
+++ james-vision/src/main/java/frc/robot/Robot.java
@@ -128,10 +128,7 @@ public class Robot extends LoggedRobot {
 
   /** This function is called once when the robot is first started up. */
   @Override
-  public void simulationInit() {
-    // Update with the simulated drivetrain pose. This should be called every loop in simulation.
-   // VisionSim.update(robotPoseMeters);
-  }
+  public void simulationInit() {}
 
   /** This function is called periodically whilst in simulation. */
   @Override
