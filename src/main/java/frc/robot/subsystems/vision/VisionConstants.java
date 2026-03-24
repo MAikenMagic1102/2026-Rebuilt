02d9a04916a4ad467afcf8f9b70ae61ace158f40
James Rittgers <JamesR2010@protonmail.com>
2026-03-23 22:21:30 -0400

    Fixed the basic translation values

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -42,13 +42,13 @@ public class VisionConstants {
 //       new Transform3d(0.0, 0.0, 0.0, new Rotation3d(0.0, 0.0, -Math.PI));
  
   public static Transform3d robotToCameraLeft =
-      new Transform3d(1.5, -1.5, 0, 
+      new Transform3d(inchesToMeters(1.5), inchesToMeters(-1.5), inchesToMeters(16), 
       new Rotation3d(0.0, 0, Units.degreesToRadians(90 + 60)));
   public static Transform3d robotToCameraCenter =
-      new Transform3d(1.5, -1.5, 16,
+      new Transform3d(inchesToMeters(1.5), inchesToMeters(-1.5), inchesToMeters(16),
       (new Rotation3d(0.0, 0, Units.degreesToRadians(90))));
   public static Transform3d robotToCameraRight = 
-      new Transform3d(1.5, -1.5, 0,
+      new Transform3d(inchesToMeters(1.5), inchesToMeters(-1.5), inchesToMeters(16),
       new Rotation3d(0.0, 0, Units.degreesToRadians(90 - 60)));
 
   // Basic filtering thresholds
