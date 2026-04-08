3acbb64a2ca81caac8bf5eb1b72d3cfabd6c422f
NeoLoonz <madisonlunna@icloud.com>
2026-04-07 22:50:59 -0400

    Code 4/7/26 code fix csdt BLDRIVE ID 24 NOW and comment out bline due to severe error for.... whatever reason

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,10 +25,10 @@ import edu.wpi.first.wpilibj2.command.Commands;
 
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-import frc.robot.autos.BackupShoot;
+// import frc.robot.autos.*;
 import frc.robot.game_util.FieldConstants.Hub;
 import frc.robot.generated.TunerConstants;
-import frc.robot.lib.BLine.FollowPath;
+// import frc.robot.lib.BLine.FollowPath;
 // import frc.robot.subsystems.AutoAlignComand;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Drumm.Drumm;
@@ -111,8 +111,9 @@ public class RobotContainer {
         // BLINE EVENT TRIGGERS HERE
         //FollowPath.registerEventTrigger("intakeOn", intakeManager.deployIntake());
         //FollowPath.registerEventTrigger("intakeOff", intakeManager.stopRoller());
-        FollowPath.registerEventTrigger("ShooterOn", drumm.DRUMMGO());
-        FollowPath.registerEventTrigger("FeederOn", feeder.FeederOut());
+        // FollowPath.registerEventTrigger("ShooterOn", drumm.DRUMMGO());
+        // FollowPath.registerEventTrigger("FeederOn", feeder.FeederOut());
+        // FollowPath.registerEventTrigger("Shoot10sec", new Shoot10sec( drumm, feeder ));
 
         
 
@@ -151,8 +152,8 @@ public class RobotContainer {
     }
 
     private void configureAutoChooser() {
-        autoChooser.setDefaultOption("Do Nothing", Commands.none());
-        autoChooser.addOption("BackUp Shoot", new BackupShoot(drivetrain).getAutoCommand());
+        // autoChooser.setDefaultOption("Do Nothing", Commands.none());
+        // autoChooser.addOption("BackUpShoot", new BackupShoot(drivetrain).getAutoCommand());
         SmartDashboard.putData("Auto Chooser", autoChooser);
     }
 
--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -149,7 +149,7 @@ public class TunerConstants {
     private static final Distance kFrontRightYPos = Inches.of(-10.5);
 
     // Back Left
-    private static final int kBackLeftDriveMotorId = 1;
+    private static final int kBackLeftDriveMotorId = 24;
     private static final int kBackLeftSteerMotorId = 27;
     private static final int kBackLeftEncoderId = 21;
     private static final Angle kBackLeftEncoderOffset = Rotations.of(0.415283203125);
