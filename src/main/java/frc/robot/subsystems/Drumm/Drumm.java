8d04e2feae9aa72a77cda7ab4e49258897a607ac
James Rittgers <JamesR2010@protonmail.com>
2026-04-08 22:34:43 -0400

    4/8/26

--- src/main/java/frc/robot/subsystems/Drumm/Drumm.java
+++ src/main/java/frc/robot/subsystems/Drumm/Drumm.java
@@ -78,7 +78,7 @@ public class Drumm extends SubsystemBase {
     }
 
     public void DrummOut(){
-      DrummL.setVoltage(-9);
+      DrummL.setVoltage(-6.3);
       // DrummR.setVoltage(12);
 
     }
--- src/main/java/frc/robot/subsystems/Drumm/DrummConstants.java
+++ src/main/java/frc/robot/subsystems/Drumm/DrummConstants.java
@@ -8,9 +8,22 @@ public class DrummConstants {
     static {
         // PLACEHOLDER VALUES — must be tuned on the real robot!
         // Format: kVoltageMap.put(distanceMeters, voltage);
-        kVoltageMap.put(1.889125, -6.0);
-        kVoltageMap.put(2.193925, -7.0);
-        kVoltageMap.put(2.498725, -8.0);
+        kVoltageMap.put(1.889125, -0.0); //3ft
+        kVoltageMap.put(2.193925, -6.0); //4ft
+        kVoltageMap.put(2.498725, -0.0); //5ft
+        kVoltageMap.put(2.803525, -0.0); //6ft
+        kVoltageMap.put(3.108325, -0.0); //7ft
+        kVoltageMap.put(3.413125, -0.0); //8ft
+        kVoltageMap.put(3.717925, -7.75); //9ft
+        kVoltageMap.put(4.022725, -0.0); //10ft
+        kVoltageMap.put(4.327525, -0.0); //11ft
+        kVoltageMap.put(4.632325, -0.0); //12ft
+        kVoltageMap.put(4.937125, -0.0); //13ft
+        kVoltageMap.put(5.241925, -0.0); //14ft
+        kVoltageMap.put(5.546725, -0.0); //15ft
+
+
+
     }
     
    
