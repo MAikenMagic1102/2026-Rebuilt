a454075ad74ec391c9bcaa49a0c6022160acdf57
GTaylorDragon <garrett.t.taylor@outlook.com>
2026-09-16 19:48:11 -0400

    Hood updates

--- src/main/java/frc/robot/subsystems/Hood/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood/Hood.java
@@ -1,11 +1,22 @@
 package frc.robot.subsystems.Hood;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 
-public class Hood {
+public class Hood extends SubsystemBase{
     public static TalonFX hood_motor = new TalonFX(0 ,"rio");
-
+    public static CANcoder hoodCANcoder = new CANcoder(0,"rio");
+    
+    
+    private DutyCycleOut dutyOut = new DutyCycleOut(0);
+    private PositionVoltage posVoltage = new PositionVoltage(0);
+    private MotionMagicVoltage mmVoltage = new MotionMagicVoltage(0);
 
 
     private enum scoreTarget {
@@ -16,13 +27,44 @@ public class Hood {
 
     };
 
-    private scoreTarget currenTarget = scoreTarget.Home;
+    private scoreTarget currentAngleTarget = scoreTarget.Home;
 
     private double HoodTargetAngle = 0.0;
-     public double getAngleDegrees(){
-    return Units.rotationsToDegrees(armCaNcoder.getPosition().getValueAsDouble() / ArmConstants.armGearingCANcoder);
+
+    boolean closedLoop = false;
+
+
+    public double getAngleDegrees(){
+        return Units.rotationsToDegrees(hoodCANcoder.getPosition().getValueAsDouble() / HoodConstants.hoodCANcoderGearing);
+    }
+    public boolean HoodAtScoring(){
+        return getAngleDegrees() < 15;
+    }
+
+  public boolean HoodAtHome(){
+    return getAngleDegrees() > 10 && getAngleDegrees() < 12;
+  }
+  
+  public boolean atGoal(){
+    return Math.abs(HoodTargetAngle - getAngleDegrees()) < HoodConstants.positionTolerence;
   }
 
+  public void setOpenLoop(double demand){
+    dutyOut.withOutput(demand);
+    hood_motor.setControl(dutyOut);
+    closedLoop = false;
+  }
 
+  public void setAnglePosition(double angle){
+    HoodTargetAngle = angle;
+    posVoltage.withPosition(Units.degreesToRotations(angle));
+    hood_motor.setControl(posVoltage);
+    closedLoop = true;
+  }
+
+  public Command setAngle(double angle){
+    return runOnce(() -> setAnglePosition(angle));
+  }
+  
 
 }
--- src/main/java/frc/robot/subsystems/Hood/HoodConstants.java
+++ src/main/java/frc/robot/subsystems/Hood/HoodConstants.java
@@ -2,6 +2,16 @@ package frc.robot.subsystems.Hood;
 
 public class HoodConstants {
     public static double HoodMaxAngle =  55.5;
-    public static double HoodMinHomeAngle = 10.5;
+    public static double HoodMinAngle = 10.5;
     public static double hoodGearing = 6.667;
+    public static double hoodCANcoderGearing = 2.5;
+
+    public static double positionTolerence = 5;
+
+    public static double HoodPos1 = 20.5;
+    public static double HoodPos2 = 30.5;
+    public static double HoodPos3 = 40.5;
+
+
+
 }
