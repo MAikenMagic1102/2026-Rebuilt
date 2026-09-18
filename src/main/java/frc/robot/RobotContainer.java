470b707d8d5de4ce61c0e031b3e811d0d5c7e8e3
GTaylorDragon <garrett.t.taylor@outlook.com>
2026-09-18 00:12:48 -0400

    Speed fixed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -75,7 +75,7 @@ public class RobotContainer {
 
     
     
-    private double MaxSpeed = 0.25 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+    private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond) * 0.5; // 3/4 of a rotation per second max angular velocity
     /* Setting up bindings for necessary control of the swerve drive platform */
     private AutoAlignComand auoalignCommand = new AutoAlignComand();
