f939880ce27057fe8ac6b8ab3fac228af58c81ed
GTaylorDragon <garrett.t.taylor@outlook.com>
2026-09-17 23:13:20 -0400

    9-17-26 EOM Fixed Button Bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -259,11 +259,11 @@ public class RobotContainer {
        
         joystick2.rightBumper().onTrue(drumm.DRUMMCLEAN()).onFalse(drumm.DRUMMNO());
 
-        joystick2.leftTrigger().onTrue(intake.OUT()).onFalse(intake.STOP());
-        joystick2.rightTrigger().onTrue(feeder.FeederFeed()).onFalse(feeder.FeederStop());
+        joystick.leftTrigger().onTrue(intake.OUT()).onFalse(intake.STOP());
+        joystick.rightTrigger().onTrue(feeder.FeederFeed().alongWith(floor.FloorOn())).onFalse(feeder.FeederStop().alongWith(floor.FloorStop()));
 
         joystick.leftBumper().onTrue(pivot.PUP()).onFalse(pivot.PSTOP());
-        joystick.rightBumper().onTrue(pivot.PDOWN().alongWith(floor.FloorOn())).onFalse(pivot.PSTOP().alongWith(floor.FloorStop()));
+        joystick.rightBumper().onTrue(pivot.PDOWN()).onFalse(pivot.PSTOP());
 
         joystick.x().onTrue(drumm.DRUMM4())
         // THIS STUFF IS VISION CODE
